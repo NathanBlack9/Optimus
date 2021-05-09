8e2127764528bd1d3f406bdb1a9c111a1846631d
Suren <suren.khachatryan1999@bk.ru>
2021-05-09 11:55:04 +0300

    added name on page with login

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-// const mysql = require('mysql');
 import express from 'express';
 import path from 'path';
 //--contact-form
 import contact from './routes/contact.js';
 //--login-form
-import login from './routes/auth.js';
+import auth from './routes/auth.js';
 
 const __dirname = path.resolve();
 const port = process.env.port || 8080;
 const app = express();
-
 
 app.use(express.static(__dirname + '/build'));
 app.use('/styles', express.static(__dirname + 'build/styles'))
@@ -31,26 +29,15 @@ app
 //-------
 
 app.use(express.json());
-app.use('/login', login);
+app.use('/login', auth);
 app.use('/contact', contact);
 
-// app
-//   .route('/logined')
-//   .get((req, res) => {
-//     res.render('index-login', {title: 'ОптПоставка'})
-//   })
+//-------Cookies & sessions------
 
-//-------
-// app.get('/', (req,res) =>{
-//   // res.send('asdas'); //сообщение
-//   res.sendFile(path.resolve(__dirname, 'build', 'index.html')); //файл
-// });
+
+
+//-------------------------------
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
-
-// app.use(static(__dirname + '/build'));
-
-
-
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import db from './db.js';
+// import cookieParser from 'cookie-parser';
+// import session from 'express-session';
+// import morgan from 'morgan';
 
+const app = express();
 const router = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+var UserName;
 router
   .route('/')
   .post(urlencodedParser, async (req, res, next) => {
@@ -12,13 +16,15 @@ router
     const email = req.body.email,
           password = req.body.password,
           results = await db.promise().query(`select * from Auth`);
+
       let i = 0;
       let login = results[0][i].e_mail; //from DB
       let pass = results[0][i].password;//from DB
-    // console.log("Почта:", email,"Пароль:", password);
-    
+
       while (i < results[0].length){
         if (email === login && password === pass){
+          UserName = await db.promise().query(`select first_name from Users where id = ${results[0][i].id}`);
+          UserName = UserName[0][0].first_name;
           res.status(200).end();
           break;
         }
@@ -30,7 +36,7 @@ router
 router
   .route('/')
   .get((req, res) => {
-    res.render('index-login', {title: 'ОптПоставка'})
+    res.render('index-login', {title: 'ОптПоставка', Name: UserName})
   });
-
+  
 export default router;
