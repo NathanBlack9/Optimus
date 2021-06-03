1b1515bd61775c1754faf96aea4baff22c7ea2cf
Suren <suren.khachatryan1999@bk.ru>
2021-06-04 00:02:26 +0300

    added sort without brand sort

---
+++ src/js/modules/sort.js
@@ -0,0 +1,40 @@
+
+  
+
+// replacedNode = nav.replaceChild(nav.children[1], nav.children[0]);
+
+// nav.appendChild(replacedNode);
+
+$(".js-sort-cheap").on('click', () => {
+  let nav = document.querySelector('.catalog__body'),
+      replacedNode;
+
+  for (let i = 0; i < nav.children.length; i++) {
+    for (let j = i; j < nav.children.length; j++) {
+      if (+nav.children[i].getAttribute('data-price') > +nav.children[j].getAttribute('data-price')){
+        replacedNode = nav.replaceChild(nav.children[j], nav.children[i]);
+        insertAfter(replacedNode, nav.children[i])
+      }
+
+    }    
+  }
+});
+
+$('.js-sort-dear').on('click', () => {
+  let nav = document.querySelector('.catalog__body'),
+      replacedNode;
+
+  for (let i = 0; i < nav.children.length; i++) {
+    for (let j = i; j < nav.children.length; j++) {
+      if (+nav.children[i].getAttribute('data-price') < +nav.children[j].getAttribute('data-price')){
+        replacedNode = nav.replaceChild(nav.children[j], nav.children[i]);
+        insertAfter(replacedNode, nav.children[i])
+      }
+
+    }    
+  }
+});
+
+function insertAfter(elem, refElem ) {
+  return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
